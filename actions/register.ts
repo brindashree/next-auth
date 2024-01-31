6bdfc895579bf1a3ddfc810b9f29663f911ca42f
Brinda <brindashree29@gmail.com>
2024-01-31 21:23:52 +0530

    feat: register user to db

--- actions/register.ts
+++ actions/register.ts
@@ -2,7 +2,9 @@
 
 import { RegisterSchema } from "@/schemas";
 import * as z from "zod";
-
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+// const { default: bcrypt } = await import('bcrypt');
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
   if (!validatedFields.success) {
@@ -10,5 +12,23 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
       error: "Invalid fields!",
     };
   }
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const existingUser = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+  // TODO: SEND verification token email
   return { success: "Account created!" };
 };
