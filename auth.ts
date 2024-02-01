0e582436e667aa1d9109eb5f7f70a2ac1f7a13f7
Brinda <brindashree29@gmail.com>
2024-02-01 21:01:59 +0530

    feat: add id in session

--- auth.ts
+++ auth.ts
@@ -8,11 +8,21 @@ export const {
   handlers: { GET, POST },
   auth,
   signIn,
-  signOut
+  signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
 });
-
-
