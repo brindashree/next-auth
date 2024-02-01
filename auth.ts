d6719250461a705fd4fd3668baff36a1e1a2bca7
Brinda <brindashree29@gmail.com>
2024-02-01 21:24:43 +0530

    feat: add db values in session

--- auth.ts
+++ auth.ts
@@ -3,6 +3,15 @@ import NextAuth from "next-auth";
 
 import authConfig from "./auth.config";
 import { db } from "./lib/db";
+import { getUserById } from "./data/user";
+declare module "next-auth" {
+  interface Session {
+    user: {
+      role: string;
+      id: string;
+    };
+  }
+}
 
 export const {
   handlers: { GET, POST },
@@ -15,10 +24,16 @@ export const {
       if (token.sub && session.user) {
         session.user.id = token.sub;
       }
-
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
       return session;
     },
     async jwt({ token }) {
+      if (!token.sub) return token;
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+      token.role = existingUser.role;
       return token;
     },
   },
